530ac1711091568b6a8a6b24e3d082cd49c8a2bf
ashwinisapreknoxpo <ashwini.sapre@knoxpo.com>
2021-03-27 13:40:12 +0530

    work on lineChart and create custom compoent using @binding

---
+++ GenericDemo/ShapeView/LineChartViewNew.swift
@@ -0,0 +1,20 @@
+//
+//  LineChartViewNew.swift
+//  GenericDemo
+//
+//  Created by Knoxpo MacBook Pro on 27/03/21.
+//
+
+import SwiftUI
+
+struct LineChartViewNew: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct LineChartViewNew_Previews: PreviewProvider {
+    static var previews: some View {
+        LineChartViewNew()
+    }
+}
